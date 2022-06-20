f173836995685d2803fc48ad90db29792336023b
Mehdi Raza <razamehdi2019@gmail.com>
2022-06-20 15:29:34 -0700

    start

---
+++ android/app/src/main/kotlin/com/example/mypro/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mypro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:mypro/pages/home.dart';
+import 'package:mypro/pages/login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomeApp(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primaryColor: Colors.red,
+      ),
+      routes: {},
+    );
+  }
+}
